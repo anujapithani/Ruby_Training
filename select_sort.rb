4cbbc480dc9dcac83bc63f868a891c55a42efac7
anujapithani <86712206+anujapithani@users.noreply.github.com>
2022-04-13 10:42:14 +0530

    Add files via upload

---
+++ select_sort.rb
@@ -0,0 +1,14 @@
+def selection_sort(a)
+    n=a.length
+   for i in 0...n 
+    for j in (i+1)...n
+        if a[i]>a[j]
+            temp=a[i]
+            a[i]=a[j]
+            a[j]=temp
+        end
+    end
+   end
+   puts a
+end
+selection_sort([100, 11, 74, 58, 12,20 ,48, 80])
