08250add3d8fb6f3238e570dd032fe566347b826
anujapithani <86712206+anujapithani@users.noreply.github.com>
2022-04-17 22:20:30 +0530

    Add files via upload

---
+++ Assignment1.rb
@@ -0,0 +1,60 @@
+puts "-----------------"
+n = 1
+while n <= 5
+  puts "* " * n
+  n += 1
+end
+
+puts "-----------------"
+n = 5
+while n>=0
+  puts "* " * n
+  n -= 1
+end
+
+puts "-----------------"
+n = 1
+while n <= 5
+1.upto(n) do |i|
+print "#{i} "
+end
+puts
+n+=1
+end
+
+puts "-----------------"
+n = 5
+n.downto(0) do |i|
+  puts "#{i} " * n
+  n -= 1
+end
+
+puts "-----------------"
+puts "Star pyramid"
+puts "Enter any  number:"
+n = gets.to_i
+for i in 1..n
+    print " "*(n-i)
+      print "* "*(i)
+      puts
+    end
+for i in 1..n
+  print " "*i
+  print "* "*(n-i)
+  puts
+end
+
+puts "-----------------"
+puts "Number pyramid"
+puts "Enter any single digit number:"
+n = gets.to_i
+for i in 1..n
+    print " "*(n-i)
+      print "#{i} "*(i)
+      puts
+    end
+for i in 1..n
+  print " "*i
+  print "#{i} "*(n-i)
+  puts
+end
